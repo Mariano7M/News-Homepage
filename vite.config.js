80df3c94ff548362463092e557f9484919a2b694
Mariano <marinhoted@gmail.com>
2023-03-27 14:31:56 -0600

    Fix: resolve option on vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 	base: '/News-Homepage/',
 	plugins: [react()],
-	assetsInclude: ['/.(?:j|t)sx?$|.mjs$/'],
+	resolve: ['.js', '.ts', '.tsx', '.jsx'],
 })
