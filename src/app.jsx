7c9e9e5e3a35e6059ed0560a3ffef1233648d23a
Mariano <marinhoted@gmail.com>
2023-03-28 11:36:32 -0600

    Fix: trying adding only footer

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,22 @@
 // import MainLayout from './layouts/main-layout'
-import Header from './components/header'
+// import Header from './components/header'
 // import MainNews from './components/main-news'
 // import Aside from './components/aside-news'
 // import MoreNews from './components/more-news'
-// import Foooter from './components/footer'
+import Foooter from './components/footer'
 
 function App() {
 	return (
 		<>
-			<Header />
+			{/* <Header /> */}
 			{/* <MainLayout> */}
 			{/*
 				<MainNews />
 				<Aside />
 				<MoreNews />
-				<Foooter /> */}
+				 */}
 			{/* </MainLayout> */}
+			<Foooter />
 		</>
 	)
 }
