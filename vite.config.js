811d8f63ad9c427fb871cbf6d6a411af0fd007fa
Mariano <marinhoted@gmail.com>
2023-03-28 11:57:03 -0600

    Fix: trying adding resolve to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
 	base: '/News-Homepage/',
 	plugins: [react()],
-
+	resolve: {
+		alias: {
+			src: path.resolve('src/'),
+		},
+	},
 	build: {
 		rollupOptions: {
 			output: {
