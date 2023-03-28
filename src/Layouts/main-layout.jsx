66011986f5753360342df10afb8c8eae1157eca7
Mariano <marinhoted@gmail.com>
2023-03-28 10:51:37 -0600

    Fix: trying removing components in main layout

--- src/Layouts/main-layout.jsx
+++ src/Layouts/main-layout.jsx
@@ -1,20 +1,20 @@
 import './main-layout.css'
 
 // eslint-disable-next-line react/prop-types
-function MainLayout({ children }) {
-	const [header, main, aside, more, footer] = children
+function MainLayout() {
+	// const [header, main, aside, more, footer] = children
 
 	return (
 		<>
-			<section className='layout__header'>{header}</section>
+			<section className='layout__header'>header</section>
 			<main className='layout__content'>
 				<section className='content__container'>
-					{main}
-					<aside className='content__aside'>{aside}</aside>
+					main
+					<aside className='content__aside'>aside</aside>
 				</section>
-				<section className='content__botton-section'>{more}</section>
+				<section className='content__botton-section'>more</section>
 			</main>
-			<section className='layout__footer'>{footer}</section>
+			<section className='layout__footer'>footer</section>
 		</>
 	)
 }
--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,19 @@
 import MainLayout from './layouts/main-layout'
-import Header from './components/header'
-import MainNews from './components/main-news'
-import Aside from './components/aside-news'
-import MoreNews from './components/more-news'
-import Foooter from './components/footer'
+// import Header from './components/header'
+// import MainNews from './components/main-news'
+// import Aside from './components/aside-news'
+// import MoreNews from './components/more-news'
+// import Foooter from './components/footer'
 
 function App() {
 	return (
 		<>
 			<MainLayout>
-				<Header />
+				{/* <Header />
 				<MainNews />
 				<Aside />
 				<MoreNews />
-				<Foooter />
+				<Foooter /> */}
 			</MainLayout>
 		</>
 	)
