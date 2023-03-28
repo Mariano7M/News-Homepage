323ed3a6e8bd26398d0cfbc1a5c7d57027b375cd
Mariano <marinhoted@gmail.com>
2023-03-28 11:59:05 -0600

    Fix: trying adding resolve to vite config

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,7 @@ export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			src: path.resolve('src/'),
-		},
-	},
-	build: {
-		rollupOptions: {
-			output: {
-				sanitizeFileName: true,
-			},
+			src: path.resolve('/src'),
 		},
 	},
 })
