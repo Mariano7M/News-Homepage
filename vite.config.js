95b704afb364add5678214a7931aeb71737f2255
Mariano <marinhoted@gmail.com>
2023-03-28 12:44:16 -0600

    Fix: trying adding another resolve

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import resolve from '@rollup/plugin-node-resolve'
+import path from 'path'
 
 export default defineConfig({
 	base: '/News-Homepage/',
-	plugins: [
-		react(),
-		resolve({
-			extensions: ['.js', '.ts', '.jsx', '.tsx'],
-		}),
-	],
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 })
